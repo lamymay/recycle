4266e9136dd053cefa2e0c74844577f799c3cb41
lamymay <lamymay@outlook.com>
2019-07-09 23:41:58 +0800

    加入UI Material-UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Home from './components/trush/test/Home'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/*<SimpleBottomNavigation/>*/}
+
+            <Home/>
+
+        </div>
+    );
 }
 
 export default App;
+
+//https://zh-hans.reactjs.org/docs/getting-started.html  react 官方文档
+//https://segmentfault.com/a/1190000012921279    文档
+//https://www.jianshu.com/p/5da35f504aef    react入门须知
---
+++ src/components/trush/test/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import '../../../assets/css/index.css';
+
+class Home extends React.Component {
+    //子类必须在构造器中的第一行调用super方法，否则新建实例会报错，因为子类中没有this对象，而是继承父类的this
+    constructor(props) {
+        //父类的this
+        super(props);
+        //定义react数据
+
+        this.state = {
+            "msg": "Person组件",
+            tittle: "这是tittle",
+            color: 'red',
+            style: {
+                color: 'red',
+                fontSize: '40px'
+            }
+        }
+    }
+
+    //绑定属性 要注意
+    // class--> className
+    // for--> htmlFor
+    //   style 也是稍有差异             <div style={{"color": 'red'}}>行内样式 写法也与html有所不同</div>
+    //其他没变
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.msg}</h2>
+
+                <div title={this.state.title}>这是一个DIV</div>
+
+                <hr/>
+
+
+            </div>
+        );
+    }
+}
+export default Home;
